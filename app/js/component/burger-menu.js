5f1151a548e608dcf79ed780ebd55b4ee5b31d58
Максим <makster559553@gmail.com>
2020-10-21 23:02:32 +0300

    disabled scroll if active mob menu

--- app/js/component/burger-menu.js
+++ app/js/component/burger-menu.js
@@ -1,14 +1,29 @@
 (function ($, window, document) {
 
+    
+    function preventDefault(e) {
+    e.preventDefault();
+    }
+    
+    function disableScroll() {
+    document.body.addEventListener('touchmove', preventDefault, {passive: false});
+    }
+    
+    function enableScroll() {
+    document.body.removeEventListener('touchmove', preventDefault, {passive: false});
+    }
+
     function burgerMenu(){
         $(".js_header_nav-mob--burger").click(function(){
             let $this = $(this);
             if(!$this.hasClass("js_active")){
                 $("body").addClass("js_burger-active");
                 $this.addClass("js_active");
+                disableScroll();
             }else{
                 $("body").removeClass("js_burger-active");
                 $this.removeClass("js_active");
+                enableScroll();
             }
         })
     }
